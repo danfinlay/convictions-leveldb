c97a11c641e8100b293d07d75f2402dd3ed44ced
flyswatter <somniac@me.com>
2013-03-30 11:15:48 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,47 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  , leveldb = require('leveldb');
+
+leveldb.open("./leveldb", { create_if_missing: true }, onOpen);
+function onOpen(err, db) {
+  var key = "mykey";
+  db.put(key, "My Value!", function(err) {
+    db.get(key, function(err, value) {
+      console.dir(value); // prints: My Value!
+      db.del(key);
+    });
+  });
+}
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
